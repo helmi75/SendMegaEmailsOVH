e43952dafd6861407a04d27ffaa310991a09debd
helmi <helmichiha@gmail.com>
2023-08-23 13:55:43 +0200

    edit the page layout for improved ergonomics - add a tabs with streamlit

--- App/EmailApp.py
+++ App/EmailApp.py
@@ -3,6 +3,7 @@ import pandas as pd
 from EmailChunker import EmailChunker
 from EmailSender import EmailSender
 from crm_database import CRMDatabase
+import Client
 
 def run_email_app(app_type):
     sender_email = "contact@kingvpn.fr"
@@ -12,7 +13,7 @@ def run_email_app(app_type):
     bdd = CRMDatabase()
 
     if app_type == "Group send":
-        run_group_send(email_sender, path)
+        run_group_send(email_sender, path, bdd)
     elif app_type == "Single send":
         run_single_send(email_sender, bdd)
     elif app_type == "Manage Client":
@@ -21,25 +22,65 @@ def run_email_app(app_type):
         run_manage_template(bdd)
 
 # page group send 
-def run_group_send(email_sender, path):
+def run_group_send(email_sender, path, bdd):
     st.write("### Group send")
-    # Ajoutez votre code ici pour la fonctionnalité "Group send"
-    file = st.file_uploader("Excel file")  # upload an excel file containing email addresses
-    if file is not None:
-        serie_email_file = pd.read_excel(file)  # read the excel file with pandas
+    tab1, tab2 = st.tabs(["From excel file", "From database"])
+    with tab1:
+        # Ajoutez votre code ici pour la fonctionnalité "Group send"
+        file = st.file_uploader("Excel file")  # upload an excel file containing email addresses
+        if file is not None:
+            serie_email_file = pd.read_excel(file)  # read the excel file with pandas
+            chunker = EmailChunker(chunk_size=298)
+            chunked_email_array = chunker.chunk_emails(serie_email_file["email"])  # split the email addresses into chunks
+            moy_email = len(chunked_email_array[0])  # calculate the average number of emails per chunk
+            nbr_groupe = len(chunked_email_array)  # calculate the number of chunks
+            st.write(f" ### There are {nbr_groupe} groups of {moy_email} emails, for a total of {nbr_groupe * moy_email} emails")
+            st.write(f"### The estimated duration of sending emails is: {nbr_groupe / 24} days")    
+
+            df_to_all = pd.DataFrame(bdd.get_all_template_one_to_all(),columns=["template_name","template_content","type_template"])
+            options = df_to_all['template_name'].to_list()
+            selected_option = st.selectbox('Choose an option:', options)
+            if selected_option == 'King_VPN_template1.html':
+                with open(path+selected_option, "r") as f:
+                    html_string = f.read()
+                st.markdown(html_string, unsafe_allow_html=True)
+
+            if st.button("Send test email"):
+                for list_email in chunked_email_array[:2]:
+                    for i, email in enumerate(list_email):          
+                            email_sender.send_email( email, "testh_helmi_html", html_string)
+                    st.write("envoyé")
+            else:
+                pass
+
+    with tab2: # From database
+        st.write("### From database ")
+        serie_email_file = pd.DataFrame(bdd.get_all_clients(), columns=["id_client","email","user_name","password","recovery_token"])
         chunker = EmailChunker(chunk_size=298)
         chunked_email_array = chunker.chunk_emails(serie_email_file["email"])  # split the email addresses into chunks
         moy_email = len(chunked_email_array[0])  # calculate the average number of emails per chunk
         nbr_groupe = len(chunked_email_array)  # calculate the number of chunks
         st.write(f" ### There are {nbr_groupe} groups of {moy_email} emails, for a total of {nbr_groupe * moy_email} emails")
-        st.write(f"### The estimated duration of sending emails is: {nbr_groupe / 24} days")       
+        st.write(f"### The estimated duration of sending emails is: {nbr_groupe / 24} days") 
+        if st.button("show email list "):
+            st.write(serie_email_file)
+        #TO modifier 
         
-        options = ['King_VPN_template1.html']
+        
+        
+
+
+
+        st.write('####  0) select tempalte ') 
+        df_to_all = pd.DataFrame(bdd.get_all_template_one_to_all(),columns=["template_name","template_content","type_template"])
+        options = df_to_all['template_name'].to_list()
         selected_option = st.selectbox('Choose an option:', options)
-        if selected_option == 'King_VPN_template1.html':
-            with open(path+selected_option, "r") as f:
-                html_string = f.read()
-            st.markdown(html_string, unsafe_allow_html=True)
+        html_content = df_to_all[df_to_all["template_name"]==selected_option]["template_content"]
+        indexe = html_content.index.to_list()[0]
+        if selected_option :
+            st.markdown(html_content[indexe], unsafe_allow_html=True) 
+            
+            
 
         if st.button("Send test email"):
             for list_email in chunked_email_array[:2]:
@@ -48,6 +89,9 @@ def run_group_send(email_sender, path):
                 st.write("envoyé")
         else:
             pass
+
+        st.write('####  1) envoyé a tout le monde')
+        st.write('####  2) envoyer au restes contact pas encore envoyé')
 
 # Page sigle send 
 def run_single_send(email_sender, bdd):
@@ -81,60 +125,108 @@ def run_single_send(email_sender, bdd):
 
 # Page Manage Client
 def run_manage_client(bdd):
-    st.write("### Manage Client")
-    with st.expander("Create client"):
-        with st.form("create Form"):
-            email = st.text_input("Enter email")
-            user_name = st.text_input("Username")
-            password = st.text_input("password")
-            recovery_token = st.text_input("recovery_token")
-            create_button = st.form_submit_button("Create")            
-    if create_button:
-        # !! ajouter un test de d'insertion
-            bdd.create_client(email, user_name, password, recovery_token)
-            st.write("Username created:", user_name)
+    tab1, tab2, tab3 = st.tabs(["Manage clients","Client data", "Add clients from excel to the BDD"])
+    with tab1:
+        st.write("### Create Client")
+        with st.expander("Create client"):
+            with st.form("create Form"):
+                email = st.text_input("Enter email")
+                user_name = st.text_input("Username")
+                password = st.text_input("password")
+                recovery_token = st.text_input("recovery_token")
+                create_button = st.form_submit_button("Create")            
+        if create_button:
+            # !! ajouter un test de d'insertion
+                bdd.create_client(email, user_name, password, recovery_token)
+                st.write("Username created:", user_name)
 
-    # deleate client 
-    st.write("### Delete client")
-    with st.expander("Delete client"):
-        with st.form("Delete Form"):
-            delete_input_id = st.text_input("Enter id client")         
-            delete_button = st.form_submit_button("Delete")    
-    if delete_button:
-        if delete_input_id != "":
-            bdd.delete_client(int(delete_input_id) )
-            st.write("Username deleted:", delete_input_id)
+        # deleate client 
+        st.write("### Delete client")
+        with st.expander("Delete client"):
+            with st.form("Delete Form"):
+                delete_input_id = st.text_input("Enter id client")         
+                delete_button = st.form_submit_button("Delete")    
+        if delete_button:
+            if delete_input_id != "":
+                bdd.delete_client(int(delete_input_id) )
+                st.write("Username deleted:", delete_input_id)
+    
+    with tab2: # Client data
+        st.write("Client data ")
+        # Search client 
+        st.write("### Search client")
+        with st.expander("Search client"):
+            with st.form("Search Form"):
+                search_input = st.text_input("Enter the username or email to search")
+                search_button = st.form_submit_button("Search")
+                
+            if search_button:
+                if search_input != "":
+                    st.write("Data from :", search_input)
 
-    # Search client 
-    st.write("### Search client")
-    with st.expander("Search client"):
-        with st.form("Search Form"):
-            search_input = st.text_input("Enter the username or email to search")
-            search_button = st.form_submit_button("Search")
+            if st.button("tous les client"):
+                st.write(pd.DataFrame(bdd.get_all_clients(), columns=["id_client",
+                                                                        "email",
+                                                                        "user_name",
+                                                                        "password",
+                                                                        "recovery_token"]
+                                                                        )) 
+        
+    with tab3:
+        st.write("insert code for insert client from file")
+        #exracte data from file
+        file = st.file_uploader("Excel file")  
+             
+        if file is not None: 
+            file_extracted = pd.read_excel(file)
+            st.write(file_extracted.columns[0])
+            try :
+
+                if  file_extracted.columns[0] == "email":
+                    st.write("fichicher avec seulement les emails ")
+                elif  file_extracted.columns[1] == "username":
+                    st.write("crée un client et l'intégrer dans la base de donné")             
+                
+            except IndexError:
+                st.write("autres fichiers")           
             
-        if search_button:
-            if search_input != "":
-                st.write("Username deleted:", search_input)
+        #client = Client(email, username, password, recovery_token=None)
 
-        if st.button("tous les client"):
-            st.write(pd.DataFrame(bdd.get_all_clients(), columns=["id_client",
-                                                                    "email",
-                                                                    "user_name",
-                                                                    "password",
-                                                                    "recovery_token"]
-                                                                    ))    
+    
             
 def run_manage_template(bdd) :
-    st.write("### Manage tempalte")
+    
     # Utilisation dans Streamlit
-    file_upload = st.file_uploader("Sélectionnez le fichier HTML", type=["html"])   
-    if file_upload is not None:
-        template_name = st.text_input("Nom du template")
-        type_template = st.selectbox("Type de template", ['one-to-all', 'one-to-one'])
-        if st.button("Insérer dans la base de données"):                       
-            bdd.create_template(template_name, file_upload.getvalue().decode('utf-8'), type_template)
-            bdd.close()
-            st.success("Le template a été inséré dans la base de données avec succès.")
+    
+
+    tab1, tab2 = st.tabs(["Add tempalte html ","Manage template"])
+
+    with tab1 :
+        file_upload = st.file_uploader("Sélectionnez le fichier HTML", type=["html"])   
+        if file_upload is not None:
+            template_name = st.text_input("Nom du template")
+            type_template = st.selectbox("Type de template", ['one-to-all', 'one-to-one'])
+            if st.button("Insérer dans la base de données"):                       
+                bdd.create_template(template_name, file_upload.getvalue().decode('utf-8'), type_template)
+                bdd.close()
+                st.success("Le template a été inséré dans la base de données avec succès.")
+
+    with tab2 :
+        st.write("### Manage tempalte")
+        if st.button("show tempalte"):  
+            df_template = pd.DataFrame(bdd.get_all_template(), columns=["id_template", "name_template","type_template"])      
+            st.write(df_template)
+        if st.button("delate template "):
+            with st.expander("Delete template"):
+                with st.form("delete template"):
+                    id_template = st.number_input("Enter id_template")                    
+                    delete_button = st.form_submit_button("delete")
+                    
+            if delete_button:
+                bdd.delete_template(int(id_template))
+                bdd.close()
+                st.success("Username deleated :", df_template["id_template"]==id_template)
+
 
 
     # add a single template
