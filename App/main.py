4faec557616b5673e0166dd80be40f4a70d0907d
helmi <helmichiha@gmail.com>
2023-08-30 19:37:39 +0200

    architectural restructuring

--- App/main.py
+++ App/main.py
@@ -1,5 +1,35 @@
 import streamlit as st
-from EmailApp import run_email_app
+from EmailSender import EmailSender
+from models.client.client import  CreateClient
+from models.client.message import CreateMessage 
+from models.client.template import  CreateTemplate
+from pages.group_send import run_group_send
+from pages.single_send import run_single_send
+from pages.manage_client import run_manage_client
+from pages.manage_template import run_manage_template
+
+
+
+def run_email_app(app_type):
+    sender_email = "contact@kingvpn.fr"
+    sender_password = "Bizerte7000"
+    path = '/home/anisse9/vpn/'
+    email_sender = EmailSender(sender_email, sender_password)
+    #bdd = CRMDatabase()
+
+    message = CreateMessage() 
+    client = CreateClient() 
+    template = CreateTemplate()
+
+    if app_type == "Group send":
+        run_group_send(email_sender, client, template)
+    elif app_type == "Single send":
+        run_single_send(email_sender, template)
+    elif app_type == "Manage Client":
+        run_manage_client( client)
+    elif app_type == "Manage Template":
+        run_manage_template( template)
+
 
 
 def main():
@@ -17,5 +47,6 @@ def main():
         run_email_app("Manage Client")
     elif add_selectbox == "Manage Template":
         run_email_app("Manage Template")
+
 if __name__ == "__main__":
     main()
