7438297689288b8d1c268c2874e117528cbf64fe
helmi <helmichiha@gmail.com>
2023-10-09 16:43:22 +0200

    add a new feature get all email from a mail server

--- App/EmailSender.py
+++ App/EmailSender.py
@@ -1,7 +1,12 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
+
+import imaplib
+import email
+from email.header import decode_header
+from email.utils import parsedate_to_datetime
+
 
 
 class EmailSender:
@@ -38,3 +43,67 @@ class EmailSender:
         except Exception as e:
             # Return error message in case of failure
             return e
+        
+    def get_mail(self):
+        try:
+            # Connexion au serveur IMAP d'OVH
+            imap_server = 'ssl0.ovh.net'
+            imap_port = 993
+
+            mail = imaplib.IMAP4_SSL(imap_server, imap_port)
+            mail.login(self.sender_email, self.sender_password)
+
+            # Sélection de la boîte aux lettres
+            mail.select('INBOX')
+
+            # Recherche de tous les e-mails
+            status, email_ids = mail.search(None, 'ALL')
+
+            if status == 'OK':
+                email_ids = email_ids[0].split()
+                all_emails = []
+
+                for email_id in email_ids:
+                    status, email_data = mail.fetch(email_id, '(RFC822)')
+                    if status == 'OK':
+                        raw_email = email_data[0][1]
+                        msg = email.message_from_bytes(raw_email)
+
+                        # Extraire les informations de l'e-mail (sujet, expéditeur, etc.)
+                        subject, encoding = decode_header(msg['Subject'])[0]
+                        #subject = subject.decode(encoding or 'utf-8')
+                        from_name, from_email = email.utils.parseaddr(msg['From'])
+
+                        # Récupérer la date de réception de l'e-mail avec gestion des erreurs
+                        date_received = None
+                        if 'Date' in msg:
+                            try:
+                                date_received = email.utils.parsedate_to_datetime(msg['Date'])
+                            except Exception as date_error:
+                                print(f"Erreur lors de la récupération de la date de réception : {str(date_error)}")
+
+                        # Récupérer le contenu de l'e-mail
+                        email_content = ""
+                        if msg.is_multipart():
+                            for part in msg.walk():
+                                if part.get_content_type() == "text/plain":
+                                    email_content = part.get_payload(decode=True).decode()
+                        else:
+                            email_content = msg.get_payload(decode=True).decode()
+
+                        # Ajouter l'e-mail à la liste
+                        all_emails.append({
+                            'from_name': from_name,
+                            'from_email': from_email,
+                            'subject': subject,
+                            'date_received': date_received,
+                            'content': email_content
+                        })
+
+                return all_emails
+
+            mail.logout()
+
+        except Exception as e:
+            return str(e)
+
